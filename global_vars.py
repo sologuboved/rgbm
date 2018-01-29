6c8be9cb3d48302ccc057bb13400c38dc32f5c4d
sologuboved <sologuboved@gmail.com>
2018-01-29 17:21:30 +0300

    added folder 'found'

--- global_vars.py
+++ global_vars.py
@@ -2,6 +2,8 @@ CATALOGUE = 'http://opac.rgub.ru/opacg/guest.html'
 
 GR_JSON = "c://users/vesna/documents/werecoder/goodreads_py3/books.json"
 
+FOUND = 'found/'
+
 SHELVES = 'shelves'
 TO_READ_SHELF = 'to-read'
 AUTHOR = 'author'
