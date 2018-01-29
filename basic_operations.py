2713822f05f58cb4517f0c98952a02a338f33be7
sologuboved <sologuboved@gmail.com>
2018-01-29 16:41:13 +0300

    added prettyprinter

--- basic_operations.py
+++ basic_operations.py
@@ -1,4 +1,5 @@
 import json
+from global_vars import *
 
 
 def load_json(json_file):
@@ -9,3 +10,13 @@ def load_json(json_file):
 def dump_json(entries, json_file):
     with open(json_file, 'w') as handler:
         json.dump(entries, handler)
+
+
+def prettyprint(books):
+    index = 1
+    for book in books:
+        print(index)
+        index += 1
+        print(AUTHOR + ':', book[AUTHOR])
+        print(TITLE + ':', book[TITLE])
+        print()
