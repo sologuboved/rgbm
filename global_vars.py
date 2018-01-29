ae41b19733543864b8f0760a5b91ff0bc89299d7
sologuboved <sologuboved@gmail.com>
2018-01-29 17:48:10 +0300

    folder 'found' --> 'found soon'

--- global_vars.py
+++ global_vars.py
@@ -1,11 +1,13 @@
 CATALOGUE = 'http://opac.rgub.ru/opacg/guest.html'
 
 GR_JSON = "c://users/vesna/documents/werecoder/goodreads_py3/books.json"
+MISSING_JSON = 'missing_in_rgbm.json'
 
-FOLDER_FOUND = 'found/'
+FOLDER_FOUND = 'found_soon/'
 
 SHELVES = 'shelves'
 TO_READ_SHELF = 'to-read'
+SOONER_SHELF = 'the_sooner_the_better'
 AUTHOR = 'author'
 TITLE = 'title'
 ABBREV = 'abbreviated'
