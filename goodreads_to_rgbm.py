61d0590a9ed1571d558093dfc424dd442b2d7028
sologuboved <sologuboved@gmail.com>
2018-01-30 16:17:27 +0300

    style

--- goodreads_to_rgbm.py
+++ goodreads_to_rgbm.py
@@ -5,19 +5,24 @@ from searcher import *
 
 class Finder(Librarian):
     def __init__(self, books_json, shelf=SOONER_SHELF, starting_date=None):
+
         super(Finder, self).__init__(books_json, shelf, starting_date)
         self.namesakes = set()
 
     def find(self, starting_index=1):
+
         ind = starting_index
         total = len(self.to_read)
+
         for book in self.to_read[(ind - 1):]:
             print("%d out of %d" % (ind, total))
             author = book[AUTHOR]
             # author_abbrev = book[ABBR_AUTHOR]
             title = book[TITLE]
             title_abbrev = book[ABBR_TITLE]
+
             result = search('', title_abbrev)
+
             num_attempts = 0
             while num_attempts < MAX_ATTEMPTS:
                 if UNDERLOADED in result:
@@ -29,6 +34,7 @@ class Finder(Librarian):
                     break
             else:
                 print("!!!!!!!!!!!", author, '-', title, 'underloaded')
+
             if result == GLITCH:
                 print("!!!!!!!!!!!", author, '-', title, 'glitched')
             elif ZERO in result:
@@ -41,9 +47,11 @@ class Finder(Librarian):
                 print(author, '-', title, "is plain weird")
                 print(result)
                 print()
+
             ind += 1
 
 
 if __name__ == '__main__':
+
     finder = Finder(GR_JSON, shelf=SOONER_SHELF)
     finder.find()
