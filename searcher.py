38cd24a9bbd113cc435e6cb1f210e8ff04429c93
sologuboved <sologuboved@gmail.com>
2018-01-30 15:59:39 +0300

    style

--- searcher.py
+++ searcher.py
@@ -1,20 +1,20 @@
-import time
 import selenium.webdriver as webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
-from global_vars import CATALOGUE, GLITCH
+from time import sleep
+from global_vars import *
 
 
 def search(author, title):
     driver = webdriver.Firefox()
     # driver = webdriver.Chrome()
     driver.get(CATALOGUE)
-    time.sleep(5)
+    sleep(SHORT_SLEEP)
     actions = ActionChains(driver)
     actions.send_keys(Keys.TAB).send_keys(author).send_keys(Keys.TAB * 2).send_keys(title).send_keys(Keys.ENTER)
     actions.perform()
-    time.sleep(10)
+    sleep(LONG_SLEEP)
 
     driver.switch_to.frame(driver.find_element_by_xpath('//frame[@src="/opacg/html/frame2.xml"]'))
     try:
@@ -23,9 +23,3 @@ def search(author, title):
         result = GLITCH
     driver.close()
     return result
-
-
-if __name__ == '__main__':
-    pass
-    res = search('Diamondddddddddddddd', '')
-    print(res)
